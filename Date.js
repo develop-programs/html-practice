d287c5124f31c9c75377babfd7117590707f2fdb
shreyansh awadhiya <shreyanshawadhiya@gmail.com>
2024-11-26 21:19:05 +0530

    first commit

---
+++ Date.js
@@ -0,0 +1,4 @@
+// export allows other files to access this function
+export function date() {
+    return new Date().getDate().toLocaleString();
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+// Import and Export in JS
+// Monolithic and Modular approach
+// Monolithic = Everything in one file
+// Modular = Splitting code into multiple files
+
+// Import = To use code from another file
+// We are importing the code from the file 'Date.js'
+
+// Import is included in ES6 (ECMAScript 2015)
+const {date} = require('./Date.js');
+console.log(date());
+
